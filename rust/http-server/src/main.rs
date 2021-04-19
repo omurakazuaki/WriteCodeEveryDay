67cffc56720b0bb8c0aa592a8027204158d1d65d
omurakazuaki <omura@enrapt.jp>
2021-04-19 10:23:27 +0900

    http-server: gzip

--- rust/http-server/src/main.rs
+++ rust/http-server/src/main.rs
@@ -6,22 +6,33 @@ use std::net::{TcpListener, TcpStream, Shutdown};
 use std::time::Duration;
 use std::collections::HashMap;
 use once_cell::sync::Lazy;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize};
 use tracing::{trace, error, instrument, Level};
 use tracing_subscriber::FmtSubscriber;
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use chrono::offset::Utc;
 use chrono::DateTime;
+use flate2::Compression;
+use flate2::bufread::GzEncoder;
 
-#[derive(Debug, Serialize, Deserialize)]
+
+#[derive(Debug, Deserialize)]
+struct GzipSettings {
+    enabled: bool,
+    min_length: u64,
+    types: Vec<String>
+}
+
+#[derive(Debug, Deserialize)]
 struct Settings {
     listen: usize,
     server_name: String,
     root: String,
     index: String,
     types: HashMap<String, Vec<String>>,
-    error_pages: HashMap<usize, String>
+    error_pages: HashMap<usize, String>,
+    gzip: GzipSettings
 }
 
 impl Settings {
@@ -38,6 +49,11 @@ impl Settings {
                     index: "index.html".into(),
                     types: HashMap::new(),
                     error_pages: HashMap::new(),
+                    gzip: GzipSettings {
+                        enabled: true,
+                        min_length: 0,
+                        types: Vec::new()
+                    }
                 }
             },
         }
@@ -62,17 +78,15 @@ fn worker(stream: TcpStream) -> impl FnMut() -> Result<(), Error> {
         stream.set_read_timeout(Some(Duration::new(60, 0)))
             .expect("set_read_timeout call failed");
         let mut reader = BufReader::new(&stream);
-        let mut writer = BufWriter::new(&stream);
+        let mut writer = BufWriter::with_capacity(1024 * 16, &stream);
         loop {
-            match read_request(&mut reader) {
+            match Request::read(&mut reader) {
                 Err(_) => break,
                 Ok(result) => {
                     match result {
                         None => continue,
                         Some(req) => {
-                            let res = build_response(&req, None);
-                            writer.write(&res.to_bytes()[..])?;
-                            writer.flush()?;
+                            Response::build(&req, None).write(&mut writer)?;
                             trace!("end {:?}", std::thread::current().id());
                             if let Some(val) = req.headers.get("Connection") {
                                 if val == "close" {
@@ -99,48 +113,50 @@ struct Request {
     body: Option<Vec<u8>>
 }
 
-#[instrument]
-fn read_request(reader: &mut BufReader<&TcpStream>) -> Result<Option<Request>, Error> {
-    let first_line = &mut String::new();
-    reader.read_line(first_line)?;
-    if first_line.len() == 0 {
-        // keep alive
-        Ok(None)
-    } else {
-        let splits: Vec<&str> = first_line.split(" ").collect();
-        let mut request = Request{
-            method: splits.get(0).unwrap().trim().into(),
-            target: splits.get(1).unwrap().trim().into(),
-            version: splits.get(2).unwrap().trim().into(),
-            headers: HashMap::new(),
-            body: None
-        };
-        loop {
-            let header_line = &mut String::new();
-            reader.read_line(header_line)?;
-            if header_line == "\r\n" {
-                break;
+impl Request {
+    #[instrument]
+    fn read(reader: &mut BufReader<&TcpStream>) -> Result<Option<Self>, Error> {
+        let first_line = &mut String::new();
+        reader.read_line(first_line)?;
+        if first_line.len() == 0 {
+            // keep alive
+            Ok(None)
+        } else {
+            let splits: Vec<&str> = first_line.split(" ").collect();
+            let mut request = Request{
+                method: splits.get(0).unwrap().trim().into(),
+                target: splits.get(1).unwrap().trim().into(),
+                version: splits.get(2).unwrap().trim().into(),
+                headers: HashMap::new(),
+                body: None
+            };
+            loop {
+                let header_line = &mut String::new();
+                reader.read_line(header_line)?;
+                if header_line == "\r\n" {
+                    break;
+                }
+                let key_value: Vec<&str> = header_line.split(":").collect();
+                let key = key_value.get(0).unwrap().trim().into();
+                let val = key_value.get(1).unwrap().trim().into();
+                request.headers.insert(key, val);
             }
-            let key_value: Vec<&str> = header_line.split(":").collect();
-            let key = key_value.get(0).unwrap().trim().into();
-            let val = key_value.get(1).unwrap().trim().into();
-            request.headers.insert(key, val);
+            let len = request.headers.get("Content-Length")
+                .and_then(|v| Some(v.parse::<usize>().unwrap()))
+                .unwrap_or(0);
+            let mut read_num = 0;
+            let mut body: Vec<u8> = Vec::new();
+            while len > read_num {
+                let mut buf = [0; 1024];
+                let n = reader.read(&mut buf)?;
+                read_num += n;
+                body.extend_from_slice(&mut buf[..n]);
+            }
+            if body.len() > 0 {
+                request.body = Some(body);
+            }
+            Ok(Some(request))
         }
-        let len = request.headers.get("Content-Length")
-            .and_then(|v| Some(v.parse::<usize>().unwrap()))
-            .or(Some(0)).unwrap();
-        let mut read_num = 0;
-        let mut body: Vec<u8> = Vec::new();
-        while len > read_num {
-            let mut buf = [0; 1024];
-            let n = reader.read(&mut buf)?;
-            read_num += n;
-            body.extend_from_slice(&mut buf[..n]);
-        }
-        if body.len() > 0 {
-            request.body = Some(body);
-        }
-        Ok(Some(request))
     }
 }
 
@@ -149,123 +165,145 @@ struct Response {
     status: usize,
     message: String,
     headers: HashMap<String, String>,
-    body: Option<Vec<u8>>
+    file: Option<PathBuf>
 }
 
 impl Response {
-    pub fn new(status: usize, headers: HashMap<String, String>, body: Option<Vec<u8>>) -> Response {
+    pub fn new(status: usize, headers: HashMap<String, String>, file: Option<PathBuf>) -> Self {
         Response {
             version: "HTTP/1.1".into(),
             status: status,
             message: MESSAGES.get(&status).unwrap_or(&String::new()).clone(),
             headers: headers,
-            body: body
+            file: file
         }
     }
-    pub fn to_bytes(self) -> Vec<u8> {
+
+    #[instrument]
+    pub fn build(req: &Request, mut status: Option<usize>) -> Self {
+        trace!("read {:?}", std::thread::current().id());
+        let mut target_path = match status {
+            None => PathBuf::from(&SETTINGS.root).join(req.target.clone().trim_start_matches('/')),
+            Some(sts) => PathBuf::from(&SETTINGS.error_pages.get(&sts).unwrap_or(&String::new()))
+        };
+        match fs::metadata(&target_path) {
+            Err(_) => match status {
+                None => Response::build(req, Some(404)),
+                Some(sts) => Response::new(sts, HashMap::new(), None)
+            },
+            Ok(meta) => {
+                if meta.is_dir() {
+                    target_path.push(PathBuf::from(&SETTINGS.index));
+                }
+                match fs::metadata(&target_path) {
+                    Err(_) => match status {
+                        None => Response::build(req, Some(404)),
+                        Some(sts) => Response::new(sts, HashMap::new(), None)
+                    },
+                    Ok(meta) => {
+                        let mut headers = HashMap::new();
+                        headers.insert("Content-Length".into(), meta.len().to_string());
+                        let content_type = Response::resolve_content_type(&target_path);
+                        headers.insert("Content-Type".into(), content_type.clone());
+                        let connection = req.headers.get("Connection").unwrap_or(&"keep-alive".into()).clone();
+                        headers.insert("Connection".into(), connection);
+                        let date = Utc::now().format("%a, %d %b %Y %T GMT").to_string();
+                        headers.insert("Date".into(), date);
+                        let target_path_as_str = target_path.to_str();
+                        let modified = meta.modified();
+                        if status.is_none() && target_path_as_str.is_some() && modified.is_ok() {
+                            let datetime: DateTime<Utc> = modified.unwrap().into();
+                            let modified_as_str = datetime.format("%a, %d %b %Y %T GMT").to_string();
+                            let mut hasher = Sha256::new();
+                            hasher.input_str(&target_path_as_str.unwrap());
+                            let e_tag = format!("{}-{:x}-{:x}", &hasher.result_str()[..8], meta.len(), datetime.timestamp());
+                            headers.insert("Last-Modified".into(), modified_as_str);
+                            headers.insert("ETag".into(), e_tag);
+                            let cache_control = req.headers.get("Cache-Control");
+                            let if_none_match = req.headers.get("If-None-Match");
+                            if cache_control != Some(&"no-store".into()) {
+                                if if_none_match == headers.get("ETag") {
+                                    status = Some(304);
+                                }
+                            }
+                        }
+                        let accept_encodings: Vec<&str> = match req.headers.get("Accept-Encoding") {
+                            None => Vec::new(),
+                            Some(values) => values.split(",")
+                                .map(|v|v.trim())
+                                .collect()
+                        };
+                        if accept_encodings.contains(&"gzip") {
+                            if SETTINGS.gzip.enabled && SETTINGS.gzip.min_length <= meta.len() && SETTINGS.gzip.types.contains(&content_type) {
+                                headers.insert("Content-Encoding".into(), "gzip".into());
+                                headers.insert("Transfer-Encoding".into(), "chunked".into());
+                                headers.remove("Content-Length");
+                            }
+                        }
+                        let file = if status == Some(304) || req.method == "HEAD" {
+                            None
+                        } else {
+                            Some(target_path)
+                        };
+                        Response::new(status.unwrap_or(200), headers, file)
+                    }
+                }
+            }
+        }
+    }
+
+    fn resolve_content_type(path: &PathBuf) -> String {
+        let ext = match path.extension() {
+            None => "",
+            Some(ext) => ext.to_str().unwrap_or("")
+        };
+        match SETTINGS.types.iter().find(|(_, v)|v.contains(&ext.into())) {
+            None => String::new(),
+            Some((types, _)) => types.clone()
+        }
+    }
+
+    pub fn write(self, writer: &mut BufWriter<&TcpStream>) -> Result<(), Error> {
         let mut res_as_string = format!("{} {} {}\r\n", self.version, self.status, self.message);
         for (key, val) in self.headers.iter() {
             res_as_string.push_str(&format!("{}: {}\r\n", key, val));
         }
         res_as_string.push_str("\r\n");
         let mut res = res_as_string.as_bytes().to_vec();
-        if self.body.is_some() {
-            res.append(&mut self.body.clone().unwrap());
+        let is_chunked = self.headers.get("Transfer-Encoding") == Some(&"chunked".into());
+        if is_chunked {
+            writer.write(res_as_string.as_bytes())?;
         }
-        res
-    }
-}
-
-#[instrument]
-fn build_response(req: &Request, mut status: Option<usize>) -> Response {
-    trace!("read {:?}", std::thread::current().id());
-    let mut target_path = PathBuf::from(&SETTINGS.root);
-    let path = match status {
-        None => req.target.clone(),
-        Some(sts) => SETTINGS.error_pages.get(&sts).unwrap_or(&String::new()).clone()
-    };
-    target_path.push(&path.trim_start_matches('/'));
-    match fs::metadata(&target_path) {
-        Err(_) => match status {
-            None => build_response(req, Some(404)),
-            Some(sts) => Response::new(sts, HashMap::new(), None)
-        },
-        Ok(meta) => {
-            if meta.is_dir() {
-                target_path.push(PathBuf::from(&SETTINGS.index));
-            }
-            match fs::metadata(&target_path) {
-                Err(_) => match status {
-                    None => build_response(req, Some(404)),
-                    Some(sts) => Response::new(sts, HashMap::new(), None)
-                },
-                Ok(meta) => {
-                    let mut headers = HashMap::new();
-                    headers.insert("Content-Length".into(), meta.len().to_string());
-                    headers.insert("Content-Type".into(), resolve_content_type(&target_path));
-                    let connection = req.headers.get("Connection").unwrap_or(&"keep-alive".into()).clone();
-                    headers.insert("Connection".into(), connection);
-                    let date = Utc::now().format("%a, %d %b %Y %T GMT").to_string();
-                    headers.insert("Date".into(), date);
-                    let target_path_as_str = target_path.to_str();
-                    let modified = meta.modified();
-                    if status.is_none() && target_path_as_str.is_some() && modified.is_ok() {
-                        let datetime: DateTime<Utc> = modified.unwrap().into();
-                        let modified_as_str = datetime.format("%a, %d %b %Y %T GMT").to_string();
-                        let mut hasher = Sha256::new();
-                        hasher.input_str(&target_path_as_str.unwrap());
-                        let e_tag = format!("{}-{:x}-{:x}", &hasher.result_str()[..8], meta.len(), datetime.timestamp());
-                        headers.insert("Last-Modified".into(), modified_as_str);
-                        headers.insert("ETag".into(), e_tag);
-                        let cache_control = req.headers.get("Cache-Control").unwrap_or(&String::new()).clone();
-                        let if_none_match = req.headers.get("If-None-Match");
-                        if cache_control != "no-store" {
-                            if if_none_match == headers.get("ETag") {
-                                status = Some(304);
-                            }
-                        }
-                    }
-                    let body = if status == Some(304) || req.method == "HEAD" {
-                        Ok(None)
-                    } else {
-                        read_content(&target_path).map(|content|Some(content))
-                    };
-                    match body {
-                        Err(_) => build_response(req, Some(500)),
-                        Ok(body) => {
-                            let status = status.unwrap_or(200);
-                            Response::new(status, headers, body)
-                        }
-                    }
+        let is_gzip = self.headers.get("Content-Encoding") == Some(&"gzip".into());
+        if self.file.is_some() {
+            let mut reader: Box<dyn Read> = if is_gzip {
+                Box::new(GzEncoder::new(BufReader::new(File::open(self.file.unwrap())?), Compression::fast()))
+            } else {
+                Box::new(BufReader::new(File::open(self.file.unwrap())?))
+            };
+            loop {
+                let mut buf = [0; 1024 * 15];
+                let n = reader.read(&mut buf)?;
+                if is_chunked {
+                    writer.write(format!("{:02x}\r\n", n).as_bytes())?;
+                    writer.write(&mut buf[..n])?;
+                    writer.write("\r\n".as_bytes())?;
+                    writer.flush()?;
+                } else {
+                    res.append(&mut buf[..n].to_vec());
+                }
+                if n == 0 {
+                    break;
                 }
             }
         }
-    }
-}
-
-fn resolve_content_type(path: &PathBuf) -> String {
-    let ext = match path.extension() {
-        None => "",
-        Some(ext) => ext.to_str().unwrap_or("")
-    };
-    match SETTINGS.types.iter().find(|(_, v)|v.contains(&ext.into())) {
-        None => String::new(),
-        Some((types, _)) => types.clone()
-    }
-}
-
-fn read_content(path: &PathBuf) -> std::io::Result<Vec<u8>> {
-    let mut reader = BufReader::new(File::open(path)?);
-    let mut content: Vec<u8> = Vec::new();
-    loop {
-        let mut buf = [0; 1024];
-        let n = reader.read(&mut buf)?;
-        if n == 0 {
-            break;
+        if !is_chunked {
+            writer.write(&res[..])?;
+            writer.flush()?;
         }
-        content.append(&mut buf[..n].to_vec());
+        trace!("wrote {:?}", std::thread::current().id());
+        Ok(())
     }
-    Ok(content)
 }
 
 fn main() {
