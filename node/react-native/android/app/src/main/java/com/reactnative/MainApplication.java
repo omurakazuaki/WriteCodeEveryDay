4b1c49c553f1948cecc9a11f3cfbbd0751373840
omurakazuaki <omura@enrapt.jp>
2021-05-21 23:28:10 +0900

    react-native: [wip] fix jest

--- node/react-native/android/app/src/main/java/com/reactnative/MainApplication.java
+++ node/react-native/android/app/src/main/java/com/reactnative/MainApplication.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
