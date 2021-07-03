8a44dcc020d4d766bd19318135b428a03474c7b4
omurakazuaki <omura@enrapt.jp>
2021-07-03 23:36:40 +0900

    angular: getting started

--- node/angular/src/app/todo/todo.component.ts
+++ node/angular/src/app/todo/todo.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  todoList = [
+    {title: 'test', description: 'This is a test todo.'},
+    {title: 'Tel', description: 'This is a test todo.'},
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onTest() {
+    alert('test');
   }
 
 }
